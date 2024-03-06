8a9c114520b556d3a2ed1d645439c57e12c08828
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2024-03-06 22:00:33 +0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <p></p>;
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+  useEffect(() => {
+    getAdvice();
+  }, []);
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Another Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice in this session
+      </p>
+    </div>
+  );
 }
 
 export default App;
